1260083d28300986aee4af153c8a6dbb64f26d21
yogievan <yogievan32@gmail.com>
2023-01-04 08:04:52 +0700

    RPL upload 14

--- resources/views/navManager/header.blade.php
+++ resources/views/navManager/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
   <div class="container-fluid py-1 px-3">
     <div>
-      <h6 class="font-weight-bolder text-white mb-0">Selamat Datang! {{Auth::user() -> nama_user ?? ''}} TERCINTA</h6>
+      <h6 class="font-weight-bolder text-white mb-0">Selamat Datang! MANAJER</h6>
     </div>
     
     <div class="ms-md-auto pe-md-3 d-flex align-items-center"> 
